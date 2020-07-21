72aade49369b645ed5057b23f4ad97ecf6de3038
Mathias <davidchevez90@gmail.com>
2020-07-21 11:48:56 -0600

    Finished PlayerController

--- DevSmash/DevSmash/Controllers/PlayerController.swift
+++ DevSmash/DevSmash/Controllers/PlayerController.swift
@@ -11,20 +11,31 @@ import UIKit
 
 class PlayerController {
     
+    //MARK: - Shared Instance
     static var shared = PlayerController()
-    
+    //MARK: - SOT
     var players: [Player] = []
     
     init () {
-        //loadFromPersistentStorage()
+        loadFromPersistentStorage()
     }
     
-    func createPlayer(name: String, health: String, stamina: String, bio: String, playerImage: UIImage) {
-        let player = Player(name: name, health: health, stamina: stamina, bio: bio, playerImage: playerImage)
+    
+    //MARK: - CRUD
+    func createPlayer(name: String, health: String, stamina: String, bio: String) {
+        let player = Player(name: name, health: health, stamina: stamina, bio: bio)
         players.append(player)
-        //saveToPersistentStorage()
+        saveToPersistentStorage()
     }
     
+    func deletePlayer(player: Player) {
+        guard let index = players.firstIndex(of: player) else {return}
+        players.remove(at: index)
+        saveToPersistentStorage()
+    }
+    
+    
+    //MARK: - Persistence
     private func fileURL() -> URL {
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let fileName = "devSmash.json"
@@ -35,7 +46,21 @@ class PlayerController {
     private func saveToPersistentStorage() {
         let jsonEncoder = JSONEncoder()
         do {
-            
+            let data = try jsonEncoder.encode(players)
+            try data.write(to: fileURL())
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func loadFromPersistentStorage() {
+        let jsonDecoder = JSONDecoder()
+        do {
+            let data = try Data(contentsOf: fileURL())
+            let players = try jsonDecoder.decode([Player].self, from: data)
+            self.players = players
+        } catch {
+            print(error.localizedDescription)
         }
     }
 }
