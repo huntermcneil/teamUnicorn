f05f12247a2d6a0e82adddb48ddf4be8500c5fc4
Hunter McNeil <huntamaneil@gmail.com>
2020-07-21 12:50:52 -0600

    main storyboard started. need font imported

---
+++ DevSmash/DevSmash/Controllers/View Controllers/PlayerSelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayerSelectionViewController.swift
+//  DevSmash
+//
+//  Created by Hunter McNeil on 7/21/20.
+//  Copyright © 2020 Hunter McNeil. All rights reserved.
+//
+
+import UIKit
+
+class PlayerSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
